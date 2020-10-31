39ebeb952b07f9521cc9214856bc13e6e3ba43e4
aike <aaiikkee@gmail.com>
2020-10-31 16:49:16 +0900

    add matlab script

---
+++ matlab/delay.m
@@ -0,0 +1,18 @@
+[y, Fs] = audioread('../voice.wav');
+
+delayTime = 0.4;
+feedback = 4;
+wetLevel = 0.5;
+attenuate = 0.5;
+
+delaySample = round(delayTime * Fs); 
+wet = attenuate * [zeros(delaySample, 1); y];
+for i=2:feedback
+    currentWet = power(attenuate, i) * [zeros(delaySample, 1); wet];
+    wet = [wet; zeros(delaySample, 1)];
+    wet = wet + currentWet;
+end
+y = [y; zeros(delaySample * feedback, 1)];
+y = y + wet * wetLevel;
+
+sound(y,Fs);
---
+++ matlab/sine.m
@@ -0,0 +1,7 @@
+Fs = 44100;
+duration = 1;
+frequency = 440;
+gain = 0.3;
+phase = 0:1/Fs:duration;
+arr = gain * sin(2 * pi * frequency * phase);
+sound(arr, Fs)
